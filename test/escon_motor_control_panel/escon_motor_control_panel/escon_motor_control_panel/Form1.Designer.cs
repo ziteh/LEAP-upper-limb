701a9dca52502a0a98e04ece1c90c88786eb2d55
ZiTe <honmonoh@gmail.com>
2021-11-20 12:41:38 +0800

    Update UI (#31)

--- test/escon_motor_control_panel/escon_motor_control_panel/escon_motor_control_panel/Form1.Designer.cs
+++ test/escon_motor_control_panel/escon_motor_control_panel/escon_motor_control_panel/Form1.Designer.cs
@@ -50,7 +50,7 @@
             // comboBoxSerialPorts
             // 
             this.comboBoxSerialPorts.FormattingEnabled = true;
-            this.comboBoxSerialPorts.Location = new System.Drawing.Point(109, 74);
+            this.comboBoxSerialPorts.Location = new System.Drawing.Point(173, 115);
             this.comboBoxSerialPorts.Name = "comboBoxSerialPorts";
             this.comboBoxSerialPorts.Size = new System.Drawing.Size(154, 33);
             this.comboBoxSerialPorts.TabIndex = 0;
@@ -58,7 +58,7 @@
             // 
             // numericUpDownSerialPortBaudrate
             // 
-            this.numericUpDownSerialPortBaudrate.Location = new System.Drawing.Point(109, 143);
+            this.numericUpDownSerialPortBaudrate.Location = new System.Drawing.Point(173, 181);
             this.numericUpDownSerialPortBaudrate.Maximum = new decimal(new int[] {
             9600,
             0,
@@ -70,7 +70,7 @@
             0,
             0});
             this.numericUpDownSerialPortBaudrate.Name = "numericUpDownSerialPortBaudrate";
-            this.numericUpDownSerialPortBaudrate.Size = new System.Drawing.Size(120, 31);
+            this.numericUpDownSerialPortBaudrate.Size = new System.Drawing.Size(154, 31);
             this.numericUpDownSerialPortBaudrate.TabIndex = 1;
             this.numericUpDownSerialPortBaudrate.Value = new decimal(new int[] {
             9600,
@@ -120,7 +120,7 @@
             // 
             // trackBarPwmDutycycle
             // 
-            this.trackBarPwmDutycycle.Location = new System.Drawing.Point(625, 532);
+            this.trackBarPwmDutycycle.Location = new System.Drawing.Point(622, 295);
             this.trackBarPwmDutycycle.Maximum = 100;
             this.trackBarPwmDutycycle.Name = "trackBarPwmDutycycle";
             this.trackBarPwmDutycycle.Size = new System.Drawing.Size(254, 90);
@@ -130,7 +130,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(52, 77);
+            this.label1.Location = new System.Drawing.Point(82, 115);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(51, 25);
             this.label1.TabIndex = 4;
@@ -139,7 +139,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 145);
+            this.label2.Location = new System.Drawing.Point(42, 183);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(99, 25);
             this.label2.TabIndex = 4;
@@ -148,7 +148,7 @@
             // labelPwmDutycycle
             // 
             this.labelPwmDutycycle.AutoSize = true;
-            this.labelPwmDutycycle.Location = new System.Drawing.Point(452, 565);
+            this.labelPwmDutycycle.Location = new System.Drawing.Point(460, 319);
             this.labelPwmDutycycle.Name = "labelPwmDutycycle";
             this.labelPwmDutycycle.Size = new System.Drawing.Size(155, 25);
             this.labelPwmDutycycle.TabIndex = 4;
@@ -158,7 +158,7 @@
             // 
             this.groupBox1.Controls.Add(this.buttonEnable);
             this.groupBox1.Controls.Add(this.buttonDisable);
-            this.groupBox1.Location = new System.Drawing.Point(63, 379);
+            this.groupBox1.Location = new System.Drawing.Point(471, 77);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(166, 169);
             this.groupBox1.TabIndex = 5;
@@ -169,7 +169,7 @@
             // 
             this.groupBox2.Controls.Add(this.buttonDirCW);
             this.groupBox2.Controls.Add(this.buttonDirCCW);
-            this.groupBox2.Location = new System.Drawing.Point(250, 379);
+            this.groupBox2.Location = new System.Drawing.Point(679, 77);
             this.groupBox2.Name = "groupBox2";
             this.groupBox2.Size = new System.Drawing.Size(165, 169);
             this.groupBox2.TabIndex = 5;
@@ -178,7 +178,7 @@
             // 
             // buttonSerialPortConnect
             // 
-            this.buttonSerialPortConnect.Location = new System.Drawing.Point(338, 101);
+            this.buttonSerialPortConnect.Location = new System.Drawing.Point(173, 250);
             this.buttonSerialPortConnect.Name = "buttonSerialPortConnect";
             this.buttonSerialPortConnect.Size = new System.Drawing.Size(141, 55);
             this.buttonSerialPortConnect.TabIndex = 2;
@@ -190,7 +190,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(944, 753);
+            this.ClientSize = new System.Drawing.Size(944, 402);
             this.Controls.Add(this.buttonSerialPortConnect);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);

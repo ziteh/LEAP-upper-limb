ea171bcd83940aee1379136ab13a696f7ad74078
ZiTe <honmonoh@gmail.com>
2021-11-27 16:26:38 +0800

    Create file of code (#39)

---
+++ test/communication_package/stm32_communication_package/src/main.c
@@ -0,0 +1,5 @@
+/**
+ * @file   main.c
+ * @author ZiTe (honmonoh@gmail.com)
+ * @brief  Communication packge test.
+ */
---
+++ test/communication_package/stm32_communication_package/src/main.h
@@ -0,0 +1,5 @@
+/**
+ * @file   main.h
+ * @author ZiTe (honmonoh@gmail.com)
+ * @brief  Communication packge test.
+ */

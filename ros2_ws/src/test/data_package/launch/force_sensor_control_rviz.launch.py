2ca5416bb2ebe87118b5e182c3ea3b945450cd37
ziteh <honmonoh@gmail.com>
2021-12-04 02:05:44 -0800

    Add launch file for force sensor control RViz (#46)

---
+++ ros2_ws/src/test/data_package/launch/force_sensor_control_rviz.launch.py
@@ -0,0 +1,28 @@
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import ThisLaunchFileDir
+from launch_ros.substitutions import FindPackageShare
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription(
+        [
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(
+                    [FindPackageShare("simple_3r_arm_launch"),
+                    "/launch/simple_3r_arm_with_relative_motion.launch.py"]
+                )
+            ),
+
+            Node(
+                package="serial_port",
+                executable="read_write_from_topic_bytemultiarray"
+            ),
+
+            Node(
+                package="data_package",
+                executable="force_sensor_value_decoder"
+            )
+        ]
+    )

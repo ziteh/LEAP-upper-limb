99099863e0ce810045c65e9418f7d7b1f5c8be1e
ZiTe <honmonoh@gmail.com>
2021-12-23 12:38:55 +0800

    Add main code files (#56)

---
+++ test/full_functions_test/stm32_multi_motor_control/src/main.cpp
@@ -0,0 +1,5 @@
+/**
+ * @file   main.cpp
+ * @author ZiTe (honmonoh@gmail.com)
+ * @brief  Multi motor control with communication.
+ */
---
+++ test/full_functions_test/stm32_multi_motor_control/src/main.h
@@ -0,0 +1,5 @@
+/**
+ * @file   main.h
+ * @author ZiTe (honmonoh@gmail.com)
+ * @brief  Multi motor control with communication.
+ */

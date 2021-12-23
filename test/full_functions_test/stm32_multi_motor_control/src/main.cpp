963fa641cf3ae875c1d86911d8e9726d313d48ac
ZiTe <honmonoh@gmail.com>
2021-12-23 17:13:06 +0800

    Add motor speed function (#56)

--- test/full_functions_test/stm32_multi_motor_control/src/main.cpp
+++ test/full_functions_test/stm32_multi_motor_control/src/main.cpp
@@ -12,6 +12,9 @@ int main(void)
 
   printf("Ready\r\n");
 
+  set_motor_speed(EFE, 80);
+  set_motor_speed(SFE, 80);
+
   while (1)
   {
     set_motor_state(EFE, ToggleState);
@@ -65,4 +68,18 @@ void set_motor_direction(Joints_t joint, Direction_t dir)
     gpio_clear(port, pin);
     break;
   }
+}
+
+void set_motor_speed(Joints_t joint, uint8_t speed)
+{
+  if (speed > 100)
+  {
+    speed = 100;
+  }
+
+  auto tim = motor_speed_pwm_tim[joint];
+  auto oc = motor_speed_pwm_oc[joint];
+  uint32_t value = PWM_TIMER_PERIOD * (speed / 100.0);
+
+  timer_set_oc_value(tim, oc, value);
 }
--- test/full_functions_test/stm32_multi_motor_control/src/main.h
+++ test/full_functions_test/stm32_multi_motor_control/src/main.h
@@ -20,42 +20,43 @@
 #include <libopencm3/stm32/adc.h>
 #include <libopencm3/cm3/nvic.h>
 
-typedef enum{
+typedef enum
+{
   EFE,
   SFE
-}Joints_t;
+} Joints_t;
 
-typedef enum{
+typedef enum
+{
   Disable = 0,
   Enable = !Disable,
   ToggleState
-}EnableState_t;
+} EnableState_t;
 
-typedef enum{
+typedef enum
+{
   CW = 0,
   CCW = !CW,
   ToggleDirection
-}Direction_t;
+} Direction_t;
 
-uint32_t motor_enable_port[2] = {
-  EFE_MOTOR_ENABLE_PORT,
-  SFE_MOTOR_ENABLE_PORT
-};
+uint32_t motor_enable_port[2] = {EFE_MOTOR_ENABLE_PORT,
+                                 SFE_MOTOR_ENABLE_PORT};
 
-uint16_t motor_enable_pin[2] = {
-  EFE_MOTOR_ENABLE_PIN,
-  SFE_MOTOR_ENABLE_PIN
-};
+uint16_t motor_enable_pin[2] = {EFE_MOTOR_ENABLE_PIN,
+                                SFE_MOTOR_ENABLE_PIN};
 
-uint32_t motor_direction_port[2] = {
-  EFE_MOTOR_DIRECTION_PORT,
-  SFE_MOTOR_DIRECTION_PORT
-};
+uint32_t motor_direction_port[2] = {EFE_MOTOR_DIRECTION_PORT,
+                                    SFE_MOTOR_DIRECTION_PORT};
 
-uint16_t motor_direction_pin[2] = {
-  EFE_MOTOR_DIRECTION_PIN,
-  SFE_MOTOR_DIRECTION_PIN
-};
+uint16_t motor_direction_pin[2] = {EFE_MOTOR_DIRECTION_PIN,
+                                   SFE_MOTOR_DIRECTION_PIN};
+
+uint32_t motor_speed_pwm_tim[2] = {EFE_MOTOR_SPEED_PWM_TIM,
+                                   SFE_MOTOR_SPEED_PWM_TIM};
+
+tim_oc_id motor_speed_pwm_oc[2] = {EFE_MOTOR_SPEED_PWM_OC,
+                                   SFE_MOTOR_SPEED_PWM_OC};
 
 void clear_communication_variable(void);
 uint16_t get_adc_value(int channel);
@@ -69,5 +70,6 @@ void usart2_isr(void);
 
 void set_motor_state(Joints_t joint, EnableState_t state);
 void set_motor_direction(Joints_t joint, Direction_t dir);
+void set_motor_speed(Joints_t joint, uint8_t speed);
 
 #endif /* MAIN_H_ */

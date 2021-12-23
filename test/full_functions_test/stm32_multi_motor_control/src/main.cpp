877b70c55f15bb12ba02fc8263a04d977a304ae5
ZiTe <honmonoh@gmail.com>
2021-12-23 20:25:36 +0800

    Add USART receive parser (#56)

--- test/full_functions_test/stm32_multi_motor_control/src/main.cpp
+++ test/full_functions_test/stm32_multi_motor_control/src/main.cpp
@@ -24,15 +24,14 @@ int main(void)
   set_motor_direction(EFE, CCW);
   set_motor_direction(SFE, CCW);
 
-  int i = 0;
   while (1)
   {
-    auto a1 = get_joint_position(EFE);
-    auto a2 = get_joint_position(SFE);
+    // auto a1 = get_joint_position(EFE);
+    // auto a2 = get_joint_position(SFE);
 
-    printf("E: %d, S: %d\r\n", a1, a2);
+    // printf("E: %d, S: %d\r\n", a1, a2);
 
-    delay(100000);
+    // delay(100000);
   }
 
   return 0;
@@ -113,14 +112,115 @@ uint16_t get_joint_position(Joints_t joint)
   return ADC_DR(adc);
 }
 
+void clear_communication_variable(void)
+{
+  receive_header = 0xff;
+  receive_payload_number = 0;
+
+  /* Clear buffer. */
+  for (int i = 0; i < BUFFER_LENGTH; i++)
+  {
+    receive_buffer[i] = 0x00;
+  }
+}
+
+void data_package_parser(uint16_t data)
+{
+  if ((data & 0x80) == 0x80)
+  {
+    /* Is header or EOT symbol. */
+    receive_header = data;
+    switch (data)
+    {
+    case MOTOR_BASIC_CONTROL_HEADER:
+      receive_payload_number = MOTOR_BASIC_CONTROL_PAYLOAD_NUMBER;
+      break;
+
+    case MOTOR_POSITION_CONTROL_HEADER:
+      receive_payload_number = MOTOR_POSITION_CONTROL_PAYLOAD_NUMBER;
+      break;
+
+    case REQUEST_MOTOR_STATE_HEADER:
+      receive_payload_number = REQUEST_MOTOR_STATE_PAYLOAD_NUMBER;
+      break;
+
+    case REQUEST_FORCE_SENSOR_VALUE_HEADER:
+      receive_payload_number = REQUEST_FORCE_SENSOR_VALUE_PAYLOAD_NUMBER;
+      break;
+
+    case EOT_SYMBOL:
+      clear_communication_variable();
+      break;
+
+    default:
+      usart_send_blocking(USART2, '?');
+      usart_send_blocking(USART2, '\r');
+      usart_send_blocking(USART2, '\n');
+      break;
+    }
+  }
+  else
+  {
+    if (receive_payload_number > 0)
+    {
+      receive_buffer[receive_payload_number - 1] = data;
+      receive_payload_number--;
+      if (receive_payload_number == 0)
+      {
+        switch (receive_header)
+        {
+        case MOTOR_BASIC_CONTROL_HEADER:
+        {
+          uint8_t id = receive_buffer[3] & 0x1f;
+          uint8_t enable = receive_buffer[2] & 0x03;
+          uint8_t dircetion = (receive_buffer[2] & 0x0c) >> 2;
+          uint16_t speed = (receive_buffer[1] & 0x3f) | ((receive_buffer[0] & 0x3f) << 6);
+
+          set_motor_state((Joints_t)id, (EnableState_t)enable);
+          set_motor_direction((Joints_t)id, (Direction_t)dircetion);
+          set_motor_speed((Joints_t)id, speed * (100.0 / 4095));
+
+          break;
+        }
+
+        case MOTOR_POSITION_CONTROL_HEADER:
+        {
+          uint8_t id = receive_buffer[2] & 0x1f;
+          uint16_t position = (receive_buffer[1] & 0x3f) | ((receive_buffer[0] & 0x3f) << 6);
+          // move(position * (100.0 / 4095));
+          break;
+        }
+
+        case REQUEST_MOTOR_STATE_HEADER:
+        {
+          uint8_t id = receive_buffer[0] & 0x1f;
+          // send_motor_state(id);
+          break;
+        }
+
+        case REQUEST_FORCE_SENSOR_VALUE_HEADER:
+        {
+          uint8_t id = receive_buffer[0] & 0x07;
+          // send_force_sensor_value(id);
+          break;
+        }
+
+        default:
+          break;
+        }
+        clear_communication_variable();
+      }
+    }
+  }
+}
+
 /**
  * @brief USART2 Interrupt service routine.
  */
 void usart2_isr(void)
 {
   uint16_t data = usart_recv(USART2);
-
-  gpio_toggle(LED_PORT, LED_PIN);
+  data_package_parser(data);
 
   /* Clear RXNE(Read data register not empty) flag of USART SR(Status register). */
   USART_SR(USART2) &= ~USART_SR_RXNE;
--- test/full_functions_test/stm32_multi_motor_control/src/main.h
+++ test/full_functions_test/stm32_multi_motor_control/src/main.h
@@ -72,6 +72,8 @@ void move(uint16_t position);
 void send_motor_state(uint8_t motor_id);
 void send_force_sensor_value(uint8_t id);
 
+void data_package_parser(uint16_t data);
+
 void usart2_isr(void);
 
 void set_motor_state(Joints_t joint, EnableState_t state);

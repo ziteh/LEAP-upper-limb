2b8c922c6d054a8010ed19e3b460c9ad386795c8
ZiTe <honmonoh@gmail.com>
2021-12-31 10:25:31 +0800

    Add basic serial port feature (#56)

--- test/full_functions_test/vs_leap_up_system/vs_leap_up_system/Form1.Designer.cs
+++ test/full_functions_test/vs_leap_up_system/vs_leap_up_system/Form1.Designer.cs
@@ -28,13 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.comboBoxSerialPortName = new System.Windows.Forms.ComboBox();
+            this.buttonSerialPortConnection = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBoxSerialPortName
+            // 
+            this.comboBoxSerialPortName.FormattingEnabled = true;
+            this.comboBoxSerialPortName.Location = new System.Drawing.Point(34, 32);
+            this.comboBoxSerialPortName.Name = "comboBoxSerialPortName";
+            this.comboBoxSerialPortName.Size = new System.Drawing.Size(164, 33);
+            this.comboBoxSerialPortName.TabIndex = 0;
+            this.comboBoxSerialPortName.Click += new System.EventHandler(this.comboBoxSerialPortName_Click);
+            // 
+            // buttonSerialPortConnection
+            // 
+            this.buttonSerialPortConnection.Location = new System.Drawing.Point(34, 80);
+            this.buttonSerialPortConnection.Name = "buttonSerialPortConnection";
+            this.buttonSerialPortConnection.Size = new System.Drawing.Size(164, 50);
+            this.buttonSerialPortConnection.TabIndex = 1;
+            this.buttonSerialPortConnection.Text = "Dis/Connect";
+            this.buttonSerialPortConnection.UseVisualStyleBackColor = true;
+            this.buttonSerialPortConnection.Click += new System.EventHandler(this.buttonSerialPortConnection_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonSerialPortConnection);
+            this.Controls.Add(this.comboBoxSerialPortName);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxSerialPortName;
+        private System.Windows.Forms.Button buttonSerialPortConnection;
     }
 }
 
--- test/full_functions_test/vs_leap_up_system/vs_leap_up_system/Form1.cs
+++ test/full_functions_test/vs_leap_up_system/vs_leap_up_system/Form1.cs
@@ -7,14 +7,60 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO.Ports;
 
 namespace vs_leap_up_system
 {
     public partial class Form1 : Form
     {
+        private SerialPort serialPort = null;
         public Form1()
         {
             InitializeComponent();
+            UpdateSerialPortName();
+        }
+
+        private void buttonSerialPortConnection_Click(object sender, EventArgs e)
+        {
+            if (serialPort == null)
+            {
+                try
+                {
+                    var port = comboBoxSerialPortName.SelectedItem.ToString();
+                    serialPort = new SerialPort(port, 9600);
+                    serialPort.Open();
+                }
+                catch (Exception ex)
+                {
+                    serialPort = null;
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else
+            {
+                serialPort.Close();
+                serialPort = null;
+            }
+        }
+
+        private void UpdateSerialPortName()
+        {
+            comboBoxSerialPortName.Items.Clear();
+            var allPorts = SerialPort.GetPortNames();
+            if (allPorts.Length > 0)
+            {
+                comboBoxSerialPortName.Items.AddRange(allPorts);
+            }
+            else
+            {
+                comboBoxSerialPortName.Items.Add("Not found");
+            }
+            comboBoxSerialPortName.SelectedIndex = 0;
+        }
+
+        private void comboBoxSerialPortName_Click(object sender, EventArgs e)
+        {
+            UpdateSerialPortName();
         }
     }
 }

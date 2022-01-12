4a8cfcfe23b10832c7db6c4bf18bad9f6c0e0a34
ZiTe <honmonoh@gmail.com>
2022-01-12 20:22:55 +0800

    Add USART feature (#61)

--- test/basic_motor_position_control/stm32_pid_test/src/main.cpp
+++ test/basic_motor_position_control/stm32_pid_test/src/main.cpp
@@ -12,6 +12,9 @@
 #include <libopencm3/stm32/adc.h>
 #include <libopencm3/cm3/nvic.h>
 
+/* USART baudrate. */
+#define USART_BAUDRATE (9600)
+
 /* User LED. */
 #define LED_PORT (GPIOA)
 #define LED_PIN (GPIO5)
@@ -48,6 +51,11 @@
 
 #define CW (1)
 #define CCW (-1)
+#define SET_MOTOR_CW (gpio_clear(MOTOR_DIRECTION_PORT, MOTOR_DIRECTION_PIN))
+#define SET_MOTOR_CCW (gpio_set(MOTOR_DIRECTION_PORT, MOTOR_DIRECTION_PIN))
+
+#define SET_MOTOR_ENABLE (gpio_set(MOTOR_ENABLE_PORT, MOTOR_ENABLE_PIN))
+#define SET_MOTOR_DISENABLE (gpio_clear(MOTOR_ENABLE_PORT, MOTOR_ENABLE_PIN))
 
 #define PWM_FREQUENCY (1000) /* PWM frequency in Hz. */
 #define PWM_TIMER_PRESCALER (48 - 1)
@@ -75,6 +83,8 @@ void clock_init(void)
   rcc_periph_clock_enable(RCC_GPIOB);
   rcc_periph_clock_enable(RCC_GPIOC);
 
+  rcc_periph_clock_enable(RCC_USART2);
+
   rcc_periph_reset_pulse(RST_TIM3);
   rcc_periph_clock_enable(RCC_TIM3);
 
@@ -95,7 +105,7 @@ void motor_gpio_init(void)
   timer_set_prescaler(MOTOR_SPEED_PWM_TIM, PWM_TIMER_PRESCALER);
   timer_set_period(MOTOR_SPEED_PWM_TIM, PWM_TIMER_PERIOD);
   timer_set_oc_mode(MOTOR_SPEED_PWM_TIM, MOTOR_SPEED_PWM_OC, TIM_OCM_PWM1);
-  set_pwm_duty_cycle(50); // Set duty cycle to 50.0 %.
+  set_pwm_duty_cycle(10); // Set duty cycle to 50.0 %.
 
   timer_disable_preload(MOTOR_SPEED_PWM_TIM);
   timer_continuous_mode(MOTOR_SPEED_PWM_TIM);
@@ -152,6 +162,30 @@ void hall_sensor_init(void)
   exti_enable_request(EXTI4);
 }
 
+void usart_init(void)
+{
+  nvic_enable_irq(NVIC_USART2_IRQ);
+
+  /* Tx pin. */
+  gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO2);
+  gpio_set_output_options(GPIOA, GPIO_OTYPE_PP, GPIO_OSPEED_50MHZ, GPIO2);
+  gpio_set_af(GPIOA, GPIO_AF7, GPIO2);
+
+  /* Rx pin. */
+  gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO3);
+  gpio_set_af(GPIOA, GPIO_AF7, GPIO3);
+
+  usart_set_baudrate(USART2, USART_BAUDRATE);
+  usart_set_databits(USART2, 8);
+  usart_set_stopbits(USART2, USART_STOPBITS_1);
+  usart_set_parity(USART2, USART_PARITY_NONE);
+  usart_set_flow_control(USART2, USART_FLOWCONTROL_NONE);
+  usart_set_mode(USART2, USART_MODE_TX_RX);
+
+  usart_enable_rx_interrupt(USART2);
+  usart_enable(USART2);
+}
+
 void delay(uint32_t value)
 {
   while (value--)
@@ -163,6 +197,7 @@ void delay(uint32_t value)
 int main(void)
 {
   clock_init();
+  usart_init();
   led_init();
   button_init();
   hall_sensor_init();
@@ -172,6 +207,11 @@ int main(void)
   hall_sensor_a = gpio_get(HALL_SENSOR_A_PORT, HALL_SENSOR_A_PIN);
   hall_sensor_b = gpio_get(HALL_SENSOR_B_PORT, HALL_SENSOR_B_PIN);
   hall_sensor_c = gpio_get(HALL_SENSOR_C_PORT, HALL_SENSOR_C_PIN);
+
+  set_pwm_duty_cycle(15);
+  SET_MOTOR_CW;
+
+  usart_send_blocking(USART2, 0x41);
 
   while (1)
   {
@@ -248,5 +288,38 @@ void exti4_isr(void)
 void exti15_10_isr(void)
 {
   exti_reset_request(EXTI13);
-  gpio_toggle(LED_PORT, LED_PIN);
+  gpio_toggle(MOTOR_ENABLE_PORT, MOTOR_ENABLE_PIN);
+}
+
+void usart2_isr(void)
+{
+  uint16_t indata = usart_recv(USART2);
+  switch (indata)
+  {
+  case 0x00:
+    SET_MOTOR_DISENABLE;
+    break;
+
+  case 0x01:
+    SET_MOTOR_ENABLE;
+    break;
+
+  case 0x02:
+    SET_MOTOR_CW;
+    break;
+
+  case 0x03:
+    SET_MOTOR_CCW;
+    break;
+
+  default:
+    SET_MOTOR_DISENABLE;
+    break;
+  }
+
+  /*
+   * Clear RXNE(Read data register not empty) flag of
+   * USART SR(Status register).
+   */
+  USART_SR(USART2) &= ~USART_SR_RXNE;
 }

738e4cfed1ce656988890a06ddf96886173422ba
ZiTe <honmonoh@gmail.com>
2022-03-30 17:09:24 +0800

    Add main code for MCP2515 Arduino (#65)

--- test/stm32_mcp2515_can_bus_module_test_arduino/src/main.cpp
+++ test/stm32_mcp2515_can_bus_module_test_arduino/src/main.cpp
@@ -1,9 +1,58 @@
-#include <Arduino.h>
+/**
+ * @file   main.cpp
+ * @author ZiTe (honmonoh@gmail.com) & Dmitry
+ * @brief  Example for Arduino MCP2515 CAN interface library.
+ *         Reference: https://github.com/autowp/arduino-mcp2515
+ */
 
-void setup() {
-  // put your setup code here, to run once:
+#include <Arduino.h>
+#include "mcp2515.h"
+
+struct can_frame canMsg1;
+struct can_frame canMsg2;
+MCP2515 mcp2515(10);
+
+void setup()
+{
+  canMsg1.can_id = 0x0F6;
+  canMsg1.can_dlc = 8;
+  canMsg1.data[0] = 0x01;
+  canMsg1.data[1] = 0x02;
+  canMsg1.data[2] = 0x03;
+  canMsg1.data[3] = 0x04;
+  canMsg1.data[4] = 0x05;
+  canMsg1.data[5] = 0x06;
+  canMsg1.data[6] = 0x07;
+  canMsg1.data[7] = 0x08;
+
+  canMsg2.can_id = 0x036;
+  canMsg2.can_dlc = 8;
+  canMsg2.data[0] = 0x80;
+  canMsg2.data[1] = 0x70;
+  canMsg2.data[2] = 0x60;
+  canMsg2.data[3] = 0x50;
+  canMsg2.data[4] = 0x40;
+  canMsg2.data[5] = 0x30;
+  canMsg2.data[6] = 0x20;
+  canMsg2.data[7] = 0x10;
+
+  while (!Serial)
+    ;
+  Serial.begin(115200);
+
+  mcp2515.reset();
+  mcp2515.setBitrate(CAN_125KBPS);
+  mcp2515.setNormalMode();
+
+  Serial.println("Example: Write to CAN");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  mcp2515.sendMessage(&canMsg1);
+  mcp2515.sendMessage(&canMsg2);
+
+  Serial.println("Messages sent");
+
+  delay(100);
 }

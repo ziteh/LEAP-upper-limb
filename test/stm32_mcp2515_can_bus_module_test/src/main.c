8ffa7d71a9ce05a7454aa64dce1c5c84611d9657
ZiTe <honmonoh@gmail.com>
2022-04-04 18:02:21 +0800

    Add mcp2515 lib

--- test/stm32_mcp2515_can_bus_module_test/src/main.c
+++ test/stm32_mcp2515_can_bus_module_test/src/main.c
@@ -14,6 +14,7 @@
 #include <libopencm3/stm32/usart.h>
 #include <libopencm3/stm32/spi.h>
 #include <libopencm3/cm3/nvic.h>
+#include "mcp2515.h"
 #include "mcp2515_register_commands.h"
 
 #define UART_BAUDRATE (9600)
@@ -28,16 +29,16 @@
 #define GPIO_LED_PIN (GPIO13)
 
 #define GPIO_SPI_SCK_PORT (GPIOB)
-#define GPIO_SPI_SCK_PIN (GPIO3)
+#define GPIO_SPI_SCK_PIN (GPIO13)
 
 #define GPIO_SPI_MISO_PORT (GPIOB)
-#define GPIO_SPI_MISO_PIN (GPIO4)
+#define GPIO_SPI_MISO_PIN (GPIO14)
 
 #define GPIO_SPI_MOSI_PORT (GPIOB)
-#define GPIO_SPI_MOSI_PIN (GPIO5)
+#define GPIO_SPI_MOSI_PIN (GPIO15)
 
 #define GPIO_SPI_CS_PORT (GPIOB)
-#define GPIO_SPI_CS_PIN (GPIO10)
+#define GPIO_SPI_CS_PIN (GPIO6)
 
 void delay(uint32_t volatile value)
 {
@@ -58,10 +59,10 @@ void spi_start(void)
 void spi_end(void)
 {
   /* Wait for transfer finished. */
-  while (!(SPI_SR(SPI3) & SPI_SR_TXE))
+  while (!(SPI_SR(SPI2) & SPI_SR_TXE))
   { /* None. */
   }
-  while (SPI_SR(SPI3) & SPI_SR_BSY)
+  while (SPI_SR(SPI2) & SPI_SR_BSY)
   { /* None. */
   }
 
@@ -78,7 +79,7 @@ static void rcc_setup(void)
   rcc_periph_clock_enable(RCC_GPIOA);
   rcc_periph_clock_enable(RCC_GPIOB);
   rcc_periph_clock_enable(RCC_USART2);
-  rcc_periph_clock_enable(RCC_SPI3);
+  rcc_periph_clock_enable(RCC_SPI2);
 }
 
 static void led_setup(void)
@@ -114,22 +115,22 @@ static spi_setup(void)
   // gpio_set_af(GPIO_SPI_CS_PORT, GPIO_AF6, GPIO_SPI_CS_PIN);
 
   gpio_mode_setup(GPIO_SPI_SCK_PORT, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO_SPI_SCK_PIN);
-  gpio_set_af(GPIO_SPI_SCK_PORT, GPIO_AF6, GPIO_SPI_SCK_PIN);
+  gpio_set_af(GPIO_SPI_SCK_PORT, GPIO_AF5, GPIO_SPI_SCK_PIN);
 
   gpio_mode_setup(GPIO_SPI_MOSI_PORT, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO_SPI_MOSI_PIN);
-  gpio_set_af(GPIO_SPI_MOSI_PORT, GPIO_AF6, GPIO_SPI_MOSI_PIN);
+  gpio_set_af(GPIO_SPI_MOSI_PORT, GPIO_AF5, GPIO_SPI_MOSI_PIN);
 
   gpio_mode_setup(GPIO_SPI_MISO_PORT, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO_SPI_MISO_PIN);
-  gpio_set_af(GPIO_SPI_MISO_PORT, GPIO_AF6, GPIO_SPI_MISO_PIN);
+  gpio_set_af(GPIO_SPI_MISO_PORT, GPIO_AF5, GPIO_SPI_MISO_PIN);
 
-  spi_reset(SPI3);
-  spi_init_master(SPI3,
+  spi_reset(SPI2);
+  spi_init_master(SPI2,
                   SPI_CR1_BAUDRATE_FPCLK_DIV_64, // SCK: 562.4 kHz
                   SPI_CR1_CPOL_CLK_TO_0_WHEN_IDLE,
                   SPI_CR1_CPHA_CLK_TRANSITION_1,
                   SPI_CR1_MSBFIRST);
-  spi_fifo_reception_threshold_8bit(SPI3);
-  spi_set_data_size(SPI3, SPI_CR2_DS_8BIT);
+  spi_fifo_reception_threshold_8bit(SPI2);
+  spi_set_data_size(SPI2, SPI_CR2_DS_8BIT);
   // spi_set_full_duplex_mode(SPI3);
   // spi_set_master_mode(SPI3);
   // spi_set_baudrate_prescaler(SPI3, SPI_CR1_BR_FPCLK_DIV_32);
@@ -145,7 +146,7 @@ static spi_setup(void)
   // spi_end();
   // spi_enable_ss_output(SPI3);
 
-  spi_enable(SPI3);
+  spi_enable(SPI2);
 }
 
 uint8_t mcp2515_read_byte(uint8_t address)
@@ -265,60 +266,62 @@ void mcp2515_reset(void)
   spi_end();
 }
 
-int mcp2515_init(void)
-{
-  mcp2515_reset();
-  mcp2515_set_config_mode();
+// int mcp2515_init(void)
+// {
+//   mcp2515_reset();
+//   mcp2515_set_config_mode();
 
-  // TQ.
-  mcp2515_write_byte(CNF1, 0x03);
-  mcp2515_write_byte(CNF2, 0x80 | PHSEG1_3TQ | PRSEG_1TQ);
-  mcp2515_write_byte(CNF3, PHSEG2_3TQ);
+//   // TQ.
+//   mcp2515_write_byte(CNF1, 0x03);
+//   mcp2515_write_byte(CNF2, 0x80 | PHSEG1_3TQ | PRSEG_1TQ);
+//   mcp2515_write_byte(CNF3, PHSEG2_3TQ);
 
-  // ID.
-  mcp2515_write_byte(TXB0SIDH, 0x11);
-  mcp2515_write_byte(TXB0SIDL, 0xE0);
+//   // ID.
+//   mcp2515_write_byte(TXB0SIDH, 0x11);
+//   mcp2515_write_byte(TXB0SIDL, 0xE0);
 
-  // Clear Receive Register.
-  mcp2515_write_byte(RXB0SIDH, 0x00);
-  mcp2515_write_byte(RXB0SIDL, 0x00);
-  mcp2515_write_byte(RXB0EID8, 0x00);
-  mcp2515_write_byte(RXB0EID0, 0x00);
-  mcp2515_write_byte(RXB0CTRL, 0x40);
-  mcp2515_write_byte(RXB0DLC, DLC_8);
+//   // Clear Receive Register.
+//   mcp2515_write_byte(RXB0SIDH, 0x00);
+//   mcp2515_write_byte(RXB0SIDL, 0x00);
+//   mcp2515_write_byte(RXB0EID8, 0x00);
+//   mcp2515_write_byte(RXB0EID0, 0x00);
+//   mcp2515_write_byte(RXB0CTRL, 0x40);
+//   mcp2515_write_byte(RXB0DLC, DLC_8);
 
-  // Filter.
-  mcp2515_write_byte(RXF0SIDH, 0x11);
-  mcp2515_write_byte(RXF0SIDL, 0xE0);
+//   // Filter.
+//   mcp2515_write_byte(RXF0SIDH, 0x11);
+//   mcp2515_write_byte(RXF0SIDL, 0xE0);
 
-  // Mask.
-  mcp2515_write_byte(RXM0SIDH, 0x00);
-  mcp2515_write_byte(RXM0SIDL, 0x00);
+//   // Mask.
+//   mcp2515_write_byte(RXM0SIDH, 0x00);
+//   mcp2515_write_byte(RXM0SIDL, 0x00);
 
-  // Interrupt.
-  mcp2515_write_byte(CANINTE, 0x01);
-  mcp2515_write_byte(CANINTF, 0x00);
+//   // Interrupt.
+//   mcp2515_write_byte(CANINTE, 0x01);
+//   mcp2515_write_byte(CANINTF, 0x00);
 
-  uint8_t state = mcp2515_set_loop_mode();
-  return state;
-}
+//   uint8_t state = mcp2515_set_loop_mode();
+//   return state;
+// }
 
 int main(void)
 {
   rcc_setup();
-  led_setup();
+  // led_setup();
   usart_setup();
   spi_setup();
+
+  mcp2515_init();
   // int a = mcp2515_init();
 
   // if (a == 1)
   // {
   //   /* Ready. */
-  //   usart_send_blocking(USART2, 'R');
-  //   usart_send_blocking(USART2, 'e');
-  //   usart_send_blocking(USART2, 'a');
-  //   usart_send_blocking(USART2, 'd');
-  //   usart_send_blocking(USART2, 'y');
+    usart_send_blocking(USART2, 'R');
+    usart_send_blocking(USART2, 'e');
+    usart_send_blocking(USART2, 'a');
+    usart_send_blocking(USART2, 'd');
+    usart_send_blocking(USART2, 'y');
   // }
   // else
   // {
@@ -334,49 +337,45 @@ int main(void)
   // }
   // usart_send_blocking(USART2, '\r');
   // usart_send_blocking(USART2, '\n');
+  //
+  // uint8_t rx_data[8] = {0};
+  // uint8_t tx_data[8] = {0x01,
+  // 0x02,
+  // 0x03,
+  // 0x04,
+  // 0x05,
+  // 0x06,
+  // 0x07,
+  // 0xFF};
 
-  uint8_t rx_data[8] = {0};
-  uint8_t tx_data[8] = {0x01,
-                        0x02,
-                        0x03,
-                        0x04,
-                        0x05,
-                        0x06,
-                        0x07,
-                        0xFF};
-
+  struct can_frame rx_can_data;
+  rx_can_data.can_dlc = 8;
+  struct can_frame tx_can_data = {.can_id = 0x00,
+                                  .can_dlc = 8,
+                                  .data[0] = 0x00,
+                                  .data[1] = 0x01,
+                                  .data[2] = 0x02,
+                                  .data[3] = 0x03,
+                                  .data[4] = 0x04,
+                                  .data[5] = 0x05,
+                                  .data[6] = 0x06,
+                                  .data[7] = 0x07};
   while (1)
   {
-    delay(1000000);
-    gpio_toggle(GPIO_LED_PORT, GPIO_LED_PIN);
+#if defined TX
+    delay(100000);
+    sendMessage(TXB0, &tx_can_data);
+#elif defined RX
+    delay(2000);
+    uint8_t length = 0;
+    readMessage(RXB0, &rx_can_data);
+    length = rx_can_data.can_dlc;
 
-    mcp2515_reset();
-    delay(300);
-    uint8_t data = mcp2515_read_byte(CANSTAT);
-    usart_send_blocking(USART2, data);
-
-    // spi_set_nss_low(SPI3);
-
-    // spi_send8(SPI3, 0xA0);
-    // spi_send8(SPI3, 0);
-    // // spi_write(SPI3, 0x00);
-    // uint8_t data = spi_read8(SPI3);
-    // usart_send_blocking(USART2, data);
-
-    // #if defined(TX)
-    //     mcp2515_write_buffer(tx_data, 8);
-    //     delay(1000000);
-    //     // #elif defined(RX)
-    //     mcp2515_read_buffer(rx_data);
-    //     for (int i = 0; i < 8; i++)
-    //     {
-    //       usart_send_blocking(USART2, rx_data[i]);
-    //     }
-    //     usart_send_blocking(USART2, 0xFE);
-    // #else
-    // #errer
-    // #endif
-    //     spi_set_nss_high(SPI3);
+    for (int i = 0; i < length; i++)
+    {
+      usart_send_blocking(USART2, rx_can_data.data[i]);
+    }
+#endif
   }
 
   return 0;
